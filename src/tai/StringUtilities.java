3ed305655c8abd662fb6082946957ce423de26f5
Tai Nguyen <ng.xtai93@gmail.com>
2017-09-19 17:59:13 -0500

    add StringUtilities: generate random string

---
+++ src/tai/StringUtilities.java
@@ -0,0 +1,18 @@
+package tai;
+
+import java.util.Random;
+
+public class StringUtilities {
+
+    public static String generateRandomString(int size) {
+        final String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+}
