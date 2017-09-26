4f530faa3300e0543b8cb19464a06688feb68edf
Tai Nguyen <ng.xtai93@gmail.com>
2017-09-26 16:05:18 -0500

    add product_detail.jsp

--- src/tai/ServletProduct.java
+++ src/tai/ServletProduct.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 public class ServletProduct extends HttpServlet {
     private ProductManager pm;
+    private StringUtilities stringUtil = StringUtilities.INSTANCE;
 
     public ServletProduct() {
         pm = new ProductManager();
@@ -21,8 +22,27 @@ public class ServletProduct extends HttpServlet {
         
         Map<Integer, Product> mapProduct = pm.getListProduct(req, uriSplit[3]);
 
-        req.setAttribute("current-category", uriSplit[3]);
-        req.setAttribute("mapProduct", mapProduct);
-        req.getRequestDispatcher("/WEB-INF/jsp/product/product_browse.jsp").forward(req, res);
-    }
+        if(uriSplit.length == 4) {
+            req.setAttribute("current-category", uriSplit[3]);
+            req.setAttribute("mapProduct", mapProduct);
+            req.getRequestDispatcher("/WEB-INF/jsp/product/product_browse.jsp").forward(req, res);
+        }
+        else if(uriSplit.length == 5) {
+            if(stringUtil.isNumeric(uriSplit[4])) {
+                Integer productId = Integer.valueOf(uriSplit[4]);
+                Product product = mapProduct.get(productId);
+                if(product == null) {
+                    res.sendRedirect(req.getContextPath());
+                }
+                else {
+                    req.setAttribute("current-product", product);
+                    req.getRequestDispatcher("/WEB-INF/jsp/product/product_detail.jsp").forward(req, res);
+                }
+            }
+            else {
+                res.sendRedirect(req.getContextPath());
+            }
+            
+        }
+}
 }
