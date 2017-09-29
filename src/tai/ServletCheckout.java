c555c4cb9b98b78813ca00d28cd5afc50299b83c
Tai Nguyen <ng.xtai93@gmail.com>
2017-09-28 19:58:14 -0500

    proceed order

--- src/tai/ServletCheckout.java
+++ src/tai/ServletCheckout.java
@@ -6,6 +6,11 @@ import java.io.*;
 
 public class ServletCheckout extends HttpServlet {
 
+    private OrderManager om;
+    
+    public ServletCheckout() {
+        om = new OrderManager();
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
         throws ServletException, IOException {
@@ -17,4 +22,20 @@ public class ServletCheckout extends HttpServlet {
             req.getRequestDispatcher("/WEB-INF/jsp/checkout.jsp").forward(req, res);
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException {
+        User currentUser = (User) req.getSession().getAttribute("currentUser");
+        if(currentUser == null) {
+            res.sendRedirect(req.getContextPath() + "/login");
+        }
+
+        Order order = om.processOrderPlaced(req, res);
+
+        req.getSession().setAttribute("command-executed", "order-place");
+        req.getSession().setAttribute("order", order);
+        res.sendRedirect(req.getContextPath() + "/success");
+    }
+
 }
